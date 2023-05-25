f17ccc6076412a76164b87c6da15dfa05df14b50
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-05-25 14:54:02 -0400

    Add Rotate Array #189

--- Array/RotateArray.py
+++ Array/RotateArray.py
@@ -1,6 +1,28 @@
 def rotate(nums, k):
-    return 0
+
+    k %= len(nums)
+    # reverse entire array
+    l, r = 0, len(nums) - 1
+    while l < r:
+        nums[l], nums[r] = nums[r], nums[l]
+        l += 1
+        r -= 1
+    # reverse first k elements
+    l, r = 0, k - 1
+    while l < r:
+        nums[l], nums[r] = nums[r], nums[l]
+        l += 1
+        r -= 1
+
+    # reverse last k elements
+    l, r = k, len(nums) - 1
+    while l < r:
+        nums[l], nums[r] = nums[r], nums[l]
+        l += 1
+        r -= 1
+
+    print(nums)
 
 
-# rotate([1, 2, 3, 4, 5, 6, 7], 3)
-# rotate([-1, -100, 3, 99], 2)
+rotate([1, 2, 3, 4, 5, 6, 7], 3)
+rotate([-1, -100, 3, 99], 2)
