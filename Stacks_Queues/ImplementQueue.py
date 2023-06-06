17b58d049183bd3cc98dab2dad39db7e6abd3841
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-06-06 12:40:25 -0400

    Add Implement Queue using Stacks #232

--- Stacks_Queues/ImplementQueue.py
+++ Stacks_Queues/ImplementQueue.py
@@ -1,19 +1,32 @@
 class MyQueue:
     def __init__(self):
-        
+        self.s1 = []
+        self.s2 = []
 
     def push(self, x: int) -> None:
-        
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        self.s1.append(x)
+        while self.s2:
+            self.s1.append(self.s2.pop())
 
     def pop(self) -> int:
-        
+        return self.s1.pop()
 
     def peek(self) -> int:
-        
+        return self.s1[-1]
 
     def empty(self) -> bool:
-        
+        return not self.s1
 
+
+test = MyQueue()
+test.push(1)
+test.push(2)
+test.push(3)
+print(test.pop())
+print(test.pop())
+print(test.pop())
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
