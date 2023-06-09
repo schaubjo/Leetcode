a85bdfddf37e4058579033d74722d2ba29c09573
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-06-09 11:20:01 -0400

    Modify #2

--- Linked-List/AddTwoNumbers.py
+++ Linked-List/AddTwoNumbers.py
@@ -13,6 +13,7 @@ def AddTwoNumbers(l1, l2):
     while l1 or l2 or carry:
         v1 = l1.val if l1 else 0
         v2 = l2.val if l2 else 0
+
         # new digit
         val = v1 + v2 + carry
 
@@ -20,7 +21,7 @@ def AddTwoNumbers(l1, l2):
         val %= 10
         cur.next = ListNode(val)
 
-        # update pointer
+        # update pointers
         cur = cur.next
         l1 = l1.next if l1 else None
         l2 = l2.next if l2 else None
