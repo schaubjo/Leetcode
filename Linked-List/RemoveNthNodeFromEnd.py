2cedce138b0eb076561df287120e06679e6ed6ae
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-06-09 16:58:34 -0400

    Add #19 incorrect solution

---
+++ Linked-List/RemoveNthNodeFromEnd.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def removeNthFromEnd(head, n):
+    fast = head
+    slow = head
+
+    for i in range(n):
+        fast = fast.next
+
+    while fast:
+        if not fast.next:
+            slow.next = slow.next.next
+        fast = fast.next
+        slow = slow.next
+
+    return head
+
+
+n1 = ListNode(1)
+n2 = ListNode(2)
+n3 = ListNode(3)
+n4 = ListNode(4)
+n5 = ListNode(5)
+
+# n1.next = n2
+# n2.next = n3
+# n3.next = n4
+# n4.next = n5
+
+res = removeNthFromEnd(n1, 1)
+while res:
+    print(res.val)
+    res = res.next
