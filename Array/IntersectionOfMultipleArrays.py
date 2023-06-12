d6c4808a345916e24b8c9b766a3660569fae28e8
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-06-11 20:22:58 -0400

    Add Intersection Of Multiple Arrays #2248

---
+++ Array/IntersectionOfMultipleArrays.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+
+def intersection(nums):
+    dict1 = Counter(nums[0])
+    for i in range(1, len(nums)):
+        for n in nums[i]:
+            if n in dict1:
+                dict1[n] += 1
+
+    res = []
+    for k, v in dict1.items():
+        if v == len(nums):
+            res.append(k)
+
+    return sorted(res)
