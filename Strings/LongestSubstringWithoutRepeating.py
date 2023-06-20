42cb01b1f0169bca6891aae76476803ccdc4d746
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-06-20 18:33:37 -0400

    Add Longest Substring Without Repeating Characters #3 (M)

--- Strings/LongestSubstringWithoutRepeating.py
+++ Strings/LongestSubstringWithoutRepeating.py
@@ -1,12 +1,16 @@
 def lengthOfLongestSubstring(s):
     letters = set()
-    start = 0
-    longest = 0
-    for c in range(len(s)):
-        if s[c] in letters:
-            longest = max(c - start, longest)
-            start = c
-        else:
-            letters.add(s[c])
+    l = 0
+    res = 0
+    for r in range(len(s)):
+        while s[r] in letters:
+            letters.remove(s[l])
+            l += 1
+        letters.add(s[r])
+        res = max(res, len(letters))
+    return res
 
-    return
+
+print(lengthOfLongestSubstring("abcabcbb"))
+print(lengthOfLongestSubstring("bbbbb"))
+print(lengthOfLongestSubstring("pwwkew"))
