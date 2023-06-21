df1423d9f3a6026c3d1887a7b582c14d2da1bc36
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-06-21 13:27:50 -0400

    Modify Reverse Words III

--- Strings/ReverseWordsIII.py
+++ Strings/ReverseWordsIII.py
@@ -1,15 +1,21 @@
-def reverseWords(s):
-    i = 0
-    res = ""
-    while i < len(s):
-        j = i
-        while j < len(s) and s[j] != " ":
-            j += 1
-        word = s[i:j][::-1]
-        res += word + " "
-        i = j + 1
+# def reverseWords(s):
+#     i = 0
+#     res = ""
+#     while i < len(s):
+#         j = i
+#         while j < len(s) and s[j] != " ":
+#             j += 1
+#         word = s[i:j][::-1]
+#         res += word + " "
+#         i = j + 1
 
-    return res[:-1]
+#     return res[:-1]
+
+def reverseWords(s):
+    return " ".join(x[::-1] for x in s.split())
+    words = s.split()
+    rWords = [x[::-1] for x in words]
+    return " ".join(rWords)
 
 
 print(reverseWords("Let's take LeetCode contest"))
