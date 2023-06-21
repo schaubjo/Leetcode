bae0d066318b980f0c0b960ee4fe2ff97223d1fd
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-06-21 13:48:11 -0400

    Modify Reverse Words in a String

--- Strings/ReverseWords.py
+++ Strings/ReverseWords.py
@@ -1,20 +1,24 @@
+# def reverseWords(s):
+#     i = 0
+#     res = ""
+#     while i < len(s):
+#         while i < len(s) and s[i] == ' ':
+#             i += 1
+#         j = i + 1
+#         while j < len(s) and s[j] != ' ':
+#             j += 1
+#         word = s[i:j]
+
+#         if len(word) > 0 and not word.isspace():
+#             res = word + " " + res if len(res) > 0 else word
+
+#         i = j + 1
+
+#     return res
+
 def reverseWords(s):
-    i = 0
-    res = ""
-    while i < len(s):
-        while i < len(s) and s[i] == ' ':
-            i += 1
-        j = i + 1
-        while j < len(s) and s[j] != ' ':
-            j += 1
-        word = s[i:j]
-
-        if len(word) > 0 and not word.isspace():
-            res = word + " " + res if len(res) > 0 else word
-
-        i = j + 1
-
-    return res
+    return " ".join(reversed(s.strip().split()))
 
 
 print(reverseWords("  hello world  "))
+print(reverseWords("a good   example"))
