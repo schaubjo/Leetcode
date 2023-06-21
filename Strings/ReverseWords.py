d2e2c4d4b5f23bc4d670667d4199547bfe47f87a
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-06-21 12:38:53 -0400

    Modify Reverse Words

--- Strings/ReverseWords.py
+++ Strings/ReverseWords.py
@@ -1,21 +1,20 @@
 def reverseWords(s):
-    words = []
     i = 0
-    while i < len(s):
-        newWord = ""
-        while i < len(s) and not s[i].isspace():
-            newWord += s[i]
-            i += 1
-        if len(newWord) > 0:
-            words.append(newWord)
-        i += 1
-
-    reversedWords = words[::-1]
     res = ""
-    for word in reversedWords:
-        res += word
-        res += " "
-    return res[:-1]
+    while i < len(s):
+        while i < len(s) and s[i] == ' ':
+            i += 1
+        j = i + 1
+        while j < len(s) and s[j] != ' ':
+            j += 1
+        word = s[i:j]
+
+        if len(word) > 0 and not word.isspace():
+            res = word + " " + res if len(res) > 0 else word
+
+        i = j + 1
+
+    return res
 
 
-print(reverseWords("the sky is blue"))
+print(reverseWords("  hello world  "))
