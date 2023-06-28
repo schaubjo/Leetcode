31454c7ed6c60434f0066b226f76b26f42fe7bfc
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-06-28 19:57:48 -0400

    Add Remove Adjacent Duplicates In String #1047 (E)

---
+++ Stacks_Queues/RemoveAdjacentDuplicates.py
@@ -0,0 +1,9 @@
+def removeDuplicates(s):
+    char_stack = []
+    for c in s:
+        if len(char_stack) > 0 and char_stack[-1] == c:
+            char_stack.pop()
+        else:
+            char_stack.append(c)
+
+    return "".join(char_stack)
