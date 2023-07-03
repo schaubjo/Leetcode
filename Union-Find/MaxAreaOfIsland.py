5e150659d410d785dc172c65ea7349e75240eef3
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-07-03 13:56:50 -0400

    Add Max Area of Island #695

---
+++ Union-Find/MaxAreaOfIsland.py
@@ -0,0 +1,23 @@
+def maxAreaOfIsland(grid):
+    ROWS = len(grid)
+    COLS = len(grid[0])
+
+    def dfs(r, c):
+        if (r < 0 or c < 0 or r == ROWS
+                or c == COLS or grid[r][c] != 1):
+            return 0
+
+        grid[r][c] = -1
+        return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+
+    maximum = 0
+    for r in range(ROWS):
+        for c in range(COLS):
+            if grid[r][c] == 1:
+                maximum = max(maximum, dfs(r, c))
+
+    return maximum
+
+
+print(maxAreaOfIsland([[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], [
+    0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]))
