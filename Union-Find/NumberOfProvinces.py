cfe1e137f75a17b47913ef5464768fec7ee486fe
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-07-05 12:30:46 -0400

    Improve Number of Provinces with Union Find

--- Union-Find/NumberOfProvinces.py
+++ Union-Find/NumberOfProvinces.py
@@ -1,21 +1,51 @@
 def findCircleNum(isConnected):
     N = len(isConnected)
+    parent = [i for i in range(N)]
+    rank = [1] * (N)
 
-    def dfs(r, c):
-        if isConnected[r][c] != 1:
-            return
-        isConnected[r][c] = 0
-        for i in range(N):
-            dfs(c, i)
+    def find(n):
+        p = parent[n]
+        while parent[p] != p:
+            p = parent[p]
 
-    provinces = 0
-    for i in range(N):
-        for j in range(N):
-            if isConnected[i][j] == 1:
-                provinces += 1
-                dfs(i, j)
+        # p is now the root representative
 
-    return provinces
+        # path compression
+        q = n
+        while parent[q] != p:
+            next = parent[q]
+            parent[q] = p
+            q = next
+
+        # return the root that we found
+        return p
+
+    def union(n1, n2):
+        p1, p2 = find(n1), find(n2)
+
+        if rank[p1] > rank[p2]:
+            parent[p2] = p1
+            rank[p1] += rank[p2]
+
+        else:
+            parent[p1] = p2
+            rank[p2] += rank[p1]
+
+    # union everything together
+    ROWS = len(isConnected)
+    COLS = len(isConnected[0])
+
+    for i in range(ROWS):
+        for j in range(COLS):
+            if isConnected[i][j] == 1 and i != j and parent[i] != parent[j]:
+                union(i, j)
+
+    count = 0
+    for i in range(len(parent)):
+        if parent[i] == i:
+            count += 1
+
+    return count
 
 
 print(findCircleNum([[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
