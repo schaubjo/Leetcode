569a8d590de7972d6e60575f3a1994d54dcb46ad
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-07-18 21:02:54 -0400

    Add Insert Delete GetRandom O(1) #380 (M)

--- Binary-Search/workTest.py
+++
@@ -1,6 +0,0 @@
-def getLast(arr):
-    return arr[len(arr) - 1]
-
-
-arr = [x for x in range(5)]
-print(getLast(arr))
---
+++ Hashmap/GetRandom.py
@@ -0,0 +1,35 @@
+import random
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.numSet = set()
+        self.numList = []
+
+    def insert(self, val: int) -> bool:
+        if val in self.numSet:
+            return False
+        self.numSet.add(val)
+        self.numList.append(val)
+
+    def remove(self, val: int) -> bool:
+        if val not in self.numSet:
+            return False
+        found = 0
+        # find index in list
+        for i in range(len(self.numList)):
+            if self.numList[i] == val:
+                found = i
+                break
+
+        # swap removed value with last element in list
+        self.numList[found] = self.numList[len(self.numList) - 1]
+
+        # pop last element to remove it from list and remove from set
+        self.numList.pop()
+        self.numSet.remove(val)
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.numList)
