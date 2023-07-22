25e47495f0bf7d0c6879e82d8305a43d96948619
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-07-21 23:05:02 -0800

    Add Optimal Partition of String #2405 (M)

---
+++ Amazon/PartitionString.py
@@ -0,0 +1,17 @@
+def partitionString(s):
+    i = 0
+    charSet = set()
+    numPartitions = 1
+    for i in range(len(s)):
+        if s[i] not in charSet:
+            charSet.add(s[i])
+        else:
+            charSet.clear()
+            charSet.add(s[i])
+            numPartitions += 1
+
+    return numPartitions
+
+
+print(partitionString(s="abacaba"))
+print(partitionString(s="ssssss"))
